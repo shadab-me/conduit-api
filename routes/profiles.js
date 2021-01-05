43536123de4c3e36e7d4807794edf55738acc20e
Shadab Ali <shadabali604@gmail.com>
2021-01-05 21:12:26 +0530

    added:follower and following

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = express.Router();
 const User = require("../models/user");
 
@@ -11,6 +12,37 @@ router.get("/:username", async (req, res) => {
     console.log(e);
   }
 });
-router.post("/:username/follow", (req, res) => {});
+router.post("/:username/follow", auth, async (req, res) => {
+  try {
+    const userName = req.params.username;
+    let user = await User.findOne({ username: userName });
+    if (!user) {
+      res.json("Invalid User");
+    } else {
+      let currentUser = req.user._doc._id;
+      if (!user.followers.includes(currentUser)) {
+        const f = await User.findByIdAndUpdate(
+          currentUser,
+          { $push: { following: user._id } },
+          { new: true }
+        );
+        const u = await User.findByIdAndUpdate(
+          user._id,
+          {
+            $push: {
+              followers: currentUser,
+            },
+          },
+          { new: true }
+        );
+        res.json(f);
+      } else {
+        res.json("Already following");
+      }
+    }
+  } catch (e) {
+    res.json(e);
+  }
+});
 
 module.exports = router;
